1f9a0a32aff228ab8437920b4ec667402d5aff22
Eden <64953425+Solaireis@users.noreply.github.com>
2022-06-27 21:57:48 +0800

    coursepage approute edits

--- src/app.py
+++ src/app.py
@@ -1274,6 +1274,15 @@ def coursePage(courseID):
     print(teacherRecords)
     teacherName = teacherRecords[2]
 
+    reviews = sql_operation(table="review", mode="retrieve" , courseID=courseID)
+    # for reviewer in reviews:
+    #     reviewRating = reviews[2]
+    #     reviewComment = reviews[3]
+    #     reviewDate = reviews[4]
+    #     reviewerUserID = reviews[0]
+    #     user = sql_operation(table="user", mode="get_user_data", userID=reviewerUserID)
+    #     reviewerUsername = user[2]
+
 
     accType = imageSrcPath = None
     userPurchasedCourses = {}
@@ -1285,9 +1294,10 @@ def coursePage(courseID):
     return render_template("users/general/course_page.html",
         imageSrcPath=imageSrcPath, userPurchasedCourses=userPurchasedCourses, teacherName=teacherName, teacherProfilePath=teacherProfilePath \
         , courseID=courseID, courseName=courseName, courseDescription=courseDescription, coursePrice=coursePrice, courseCategory=courseCategory, \
-        courseRating=courseRating, courseRatingCount=courseRatingCount, courseDate=courseDate, courseVideoPath=courseVideoPath, accType=accType)
+        courseRating=courseRating, courseRatingCount=courseRatingCount, courseDate=courseDate, courseVideoPath=courseVideoPath, accType=accType,\
+             reviews=reviews)
 
-@app.route("/course-review/<courseID>")
+@app.route("/course-review/<courseID>") #writing of review
 @validate_session
 def courseReview(courseID):
     accType = imageSrcPath = None
