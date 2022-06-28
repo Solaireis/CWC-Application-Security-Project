ecb7f84dfdd01c9b46632f1973767c69e237596c
calpis <calvinlyn11@gmail.com>
2022-06-29 01:03:06 +0800

    update to software integrity

--- src/python_files/WarehouseAPI.py
+++ src/python_files/WarehouseAPI.py
@@ -22,42 +22,37 @@ packagedir.mkdir(parents=True, exist_ok=True)
 with open(dirname) as f:
     dependencies = f.readlines()
 
-# Work in progress
-# for i in dependencies:
-#     name = i.split(">")[0]
-#     try:
-#         version = i.split("<")[1]
+# Work in progress, now takes latest version (taking into account the maximum version), but still does not check for system type
 
-#     except:
-#         version = (i.split("=")[-1]).strip()
-#         datafile = (requests.get(f"https://pypi.org/pypi/{name}/json", stream=True, headers=headers, timeout=10)).json()
-#         file = datafile["releases"][version][0]
-#         url = file["url"]
-#         filename = url.split("/")[-1]
-#         hashed = file["digests"]["sha256"]
-#         path = f"{packagedir}/{filename}"
-
-#         with requests.get(url, stream=True, headers=headers, timeout=10) as r:
-#             with open(path, "wb") as f:
-#                 shutil.copyfileobj(r.raw, f)
-
-# For now does not check for version, and which whl file to download
-#Plan to take latest valid version as well
 for i in dependencies:
+    maximum = False
     sha256 = hashlib.sha256()
 
     name = i.split(">")[0]
     version = (i.split("=")[-1]).strip()
     try:
-        x = version.split(",")[1]
-        version = (version.split(",")[0]).strip()
+        version = (version.split(",")[1]).strip()
+        maximum = True
     except:
         version = version
 
     datafile = (requests.get(f"https://pypi.org/pypi/{name}/json", stream=True, headers=headers, timeout=10)).json()
+    file = datafile["releases"]
+    versions = list(file)
+
+    for j in range(len(versions)-1, -1, -1):
+        if (not maximum):
+            if (versions[j] >= version):
+                version = versions[j]
+                break
+        else:
+            if (versions[j] < version):
+                version = versions[j]
+                break
+    
     file = datafile["releases"][version][0]
+    filename = file["filename"]
     url = file["url"]
-    filename = url.split("/")[-1]
     hashed = file["digests"]["sha256"]
     path = f"{packagedir}/{filename}"
 
@@ -78,4 +73,42 @@ for i in dependencies:
         # pipmain(['install', path])
         print(f"Dependency {i} matches the hash! Successfully Installed & Deleted")
         Path(path).unlink(missing_ok=True)
+
+# For now does not check for version, and which whl file to download
+#Plan to take latest valid version as well
+# for i in dependencies:
+#     sha256 = hashlib.sha256()
+
+#     name = i.split(">")[0]
+#     version = (i.split("=")[-1]).strip()
+#     try:
+#         x = version.split(",")[1]
+#         version = (version.split(",")[0]).strip()
+#     except:
+#         version = version
+
+#     datafile = (requests.get(f"https://pypi.org/pypi/{name}/json", stream=True, headers=headers, timeout=10)).json()
+#     file = datafile["releases"][version][0]
+#     url = file["url"]
+#     filename = url.split("/")[-1]
+#     hashed = file["digests"]["sha256"]
+#     path = f"{packagedir}/{filename}"
+
+#     with requests.get(url, stream=True, headers=headers, timeout=10) as r:
+#         with open(path, "wb") as f:
+#             shutil.copyfileobj(r.raw, f)
+
+#     with open(path, 'rb') as f:
+#         data = f.read()
+#         sha256.update(data)
+
+#     if (sha256.hexdigest() != hashed):
+#         Path(path).unlink()
+#         print(f"Dependency {i} does not match the hash!")
+#     else:
+#         # Don't uncomment yet, kinda weird, it uninstalled some of my shit because i put latest version
+#         # It works but its an old way of doing it so need find improved way
+#         # pipmain(['install', path])
+#         print(f"Dependency {i} matches the hash! Successfully Installed & Deleted")
+#         Path(path).unlink(missing_ok=True)
 
