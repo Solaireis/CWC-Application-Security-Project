7ff9c479f29df843e11060a08a58378788d9d477
Eden <64953425+Solaireis@users.noreply.github.com>
2022-07-08 16:22:42 +0800

    made role class

--- src/routes/SuperAdmin.py
+++ src/routes/SuperAdmin.py
@@ -7,13 +7,14 @@ from flask import Blueprint, render_template, redirect, url_for, session
 # import local python libraries
 from python_files.functions.SQLFunctions import *
 from python_files.functions.NormalFunctions import *
+from python_files.classes.Roles import RoleInfo
 
 superAdminBP = Blueprint("adminBP", __name__, static_folder="static", template_folder="template")
 
 @superAdminBP.route("/admin-profile", methods=["GET","POST"])
 def adminDashboard():
     roles= sql_operation(table="review", mode="retrieve_all")
-
+    RoleInfo(roles)
 
     return 
 
