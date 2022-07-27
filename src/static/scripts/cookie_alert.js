78662d78a39931d4da98684f067a748ce9a1c3d3
KJHJason <kuanjunhaojason@gmail.com>
2022-07-27 20:12:23 +0800

    as we prepare for web hosting, added a modal msg

--- src/static/scripts/cookie_alert.js
+++ src/static/scripts/cookie_alert.js
@@ -1,63 +1,70 @@
-/* ---------------  Edited Cookie alert Javascript --------------- */
+/* ---------------  Cookie alert Javascript --------------- */
 
-/*
- * Bootstrap Cookie Alert by Wruczek
- * https://github.com/Wruczek/Bootstrap-Cookie-Alert
- * Released under MIT license
- */
+// Cookie functions from w3schools
+function set_cookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    // cookie attributes settings in javascript: 
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
+    document.cookie = `${cname}=${cvalue}; expires=${d.toUTCString()}; Path=/; SameSite=lax; Secure`;
+}
+
+function get_cookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
-    var cookieAlert = document.querySelector(".cookiealert");
-    var acceptCookies = document.querySelector(".acceptcookies");
+    let cookieAlert = document.querySelector(".cookiealert");
+    let acceptCookies = document.querySelector(".acceptcookies");
+    let importantAcknowledgement = new bootstrap.Modal(document.getElementById("importantModal"), {});
+    let importantAcknowledgementBtn = document.getElementById("importantModalButton");
 
-    if (!cookieAlert) {
+    if (!cookieAlert && !importantAcknowledgement) {
         return;
     }
 
-    cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
+    cookieAlert.offsetHeight; // Force browser to trigger reflow
 
-    // Show the alert if we cant find the "acceptCookies" cookie
-    if (!getCookie("acceptCookies")) {
+    // Show the alert if we cant find the "accepted_cookies" cookie
+    if (!get_cookie("accepted_cookies")) {
         cookieAlert.classList.add("show");
     }
 
-    // When clicking on the agree button, create a one month or 30 days specifically
-    // cookie to remember user's choice and close the banner
+    // Show the alert if we cant find the "risks_accepted" cookie
+    if (!get_cookie("risks_accepted")) {
+        importantAcknowledgement.show();
+    }
+
+    // When clicking on the agree button, create a 30 days
+    // cookie to remember user's choice and close the banner/modal
     acceptCookies.addEventListener("click", function () {
-        setCookie("acceptCookies", true, 30);
+        set_cookie("accepted_cookies", true, 30);
         cookieAlert.classList.remove("show");
 
         // dispatch the accept event
         window.dispatchEvent(new Event("cookieAlertAccept"));
     });
 
-    // Cookie functions from w3schools
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        // cookie attributes settings in javascript: 
-        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
-        document.cookie = `${cname}=${cvalue}; expires=${d.toUTCString()}; Path=/; SameSite=lax; Secure`;
-    }
+    importantAcknowledgementBtn.addEventListener("click", function () {
+        set_cookie("risks_accepted", true, 30);
 
-    // TODO: Check this javascript function if it is vulnerable to XSS
-    function getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(";");
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === " ") {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
+        // dispatch the accept event
+        window.dispatchEvent(new Event("serviceAccepted"));
+    });
 });
 
 /* ---------------  End of Edited Cookie warning alert Javascript --------------- */
