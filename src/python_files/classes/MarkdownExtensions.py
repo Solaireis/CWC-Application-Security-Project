7c361a601b4142efc3a98f0c64da083a64c0e4de
KJHJason <kuanjunhaojason@gmail.com>
2022-07-31 14:30:55 +0800

    edited markdown extensions

--- src/python_files/classes/MarkdownExtensions.py
+++ src/python_files/classes/MarkdownExtensions.py
@@ -1,5 +1,5 @@
 # import third party libraries
-import markdown
+from markdown import preprocessors, extensions, Markdown
 
 # import local python libraries
 from .Constants import CONSTANTS
@@ -14,9 +14,9 @@ class UsefulRegexForMarkdown:
     MARKDOWN_ANCHOR_REGEX: re.Pattern[str] = re.compile(r"\[(.*?)\]\((.*?)\)")
     HTML_ANCHOR_REGEX: re.Pattern[str] = re.compile(r"(&lt;a.*?)(href=)(&#x27;|&quot;)(.*?)(&#x27;|&quot;)(.*?)(&gt;)(.*?)(&lt;/a&gt;)")
 
-class AnchorTagPreprocessor(markdown.preprocessors.Preprocessor):
+class AnchorTagPreprocessor(preprocessors.Preprocessor):
     """Add attributes to the anchor html tag or the anchor markdown syntax."""
-    def run(self, lines):
+    def run(self, lines:list) -> list:
         newLines = []
         for line in lines:
             htmlAnchorTagArray = UsefulRegexForMarkdown.HTML_ANCHOR_REGEX.findall(line)
@@ -24,7 +24,7 @@ class AnchorTagPreprocessor(markdown.preprocessors.Preprocessor):
                 for htmlAnchorTag in htmlAnchorTagArray:
                     line = line.replace(
                         "".join(htmlAnchorTag),
-                        fr"<a rel='nofollow' href='{CONSTANTS.REDIRECT_CONFIRMATION_URL}?{CONSTANTS.REDIRECT_CONFIRMATION_PARAM_NAME}={htmlAnchorTag[3]}'>{htmlAnchorTag[-2]}</a>"
+                        fr"<a rel='nofollow' target='_blank' href='{CONSTANTS.REDIRECT_CONFIRMATION_URL}?{CONSTANTS.REDIRECT_CONFIRMATION_PARAM_NAME}={htmlAnchorTag[3]}'>{htmlAnchorTag[-2]}</a>"
                     )
 
             markdownAnchorTagArray = UsefulRegexForMarkdown.MARKDOWN_ANCHOR_REGEX.findall(line)
@@ -33,7 +33,7 @@ class AnchorTagPreprocessor(markdown.preprocessors.Preprocessor):
                     hrefURL = markdownAnchorTag[1]
                     line = re.sub(
                         pattern=UsefulRegexForMarkdown.MARKDOWN_ANCHOR_REGEX,
-                        repl=fr"<a rel='nofollow' href='{CONSTANTS.REDIRECT_CONFIRMATION_URL}?{CONSTANTS.REDIRECT_CONFIRMATION_PARAM_NAME}={hrefURL}'>\1</a>",
+                        repl=fr"<a rel='nofollow' target='_blank' href='{CONSTANTS.REDIRECT_CONFIRMATION_URL}?{CONSTANTS.REDIRECT_CONFIRMATION_PARAM_NAME}={hrefURL}'>\1</a>",
                         string=line,
                         count=1 # max number of pattern occurrences before replacement
                     )
@@ -41,9 +41,9 @@ class AnchorTagPreprocessor(markdown.preprocessors.Preprocessor):
             newLines.append(line)
         return newLines
 
-class AnchorTagExtension(markdown.extensions.Extension):
+class AnchorTagExtension(extensions.Extension):
     """Will add rel="nofollow" before markdown conversion for HTML tags or markdown syntax."""
-    def extendMarkdown(self, md):
+    def extendMarkdown(self, md:Markdown) -> None:
         md.registerExtension(self)
         # Note that the integer in the third argument is the priority when parsing the markdown string.
         md.preprocessors.register(AnchorTagPreprocessor(md), "addAttributesToAnchorTags", 75)
