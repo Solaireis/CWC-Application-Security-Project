1526da2087ef8aff2e98e27d6a13674adb1f0f44
Eden <64953425+Solaireis@users.noreply.github.com>
2022-08-14 19:41:02 +0800

    removed unused components

--- src/static/scripts/create_course.js
+++ src/static/scripts/create_course.js
@@ -1,44 +1,4 @@
-/* // Dropzone.js for segmenting data payload to chunks of data
-Dropzone.options.courseThumbnail = {
-    maxFiles: 1,
-    paramName: 'courseThumbnail',
-    acceptedFiles: ".jpeg,.jpg,.png",
-    chunking: true,
-    forceChunking: true,
-    url: '/create_course/{{ teacherUID }}',
-    maxFilesize: 50, // megabytes
-    chunkSize: 1000000, // bytes
-    retryChunks: true,
-    retryChunksLimit: 3,
-    autoProcessQueue: false,
-    init: function() {
-        let myDropzone = this;
-        
-        myDropzone.on("addedfile", function() {
-            document.querySelector(".dz-progress").classList.add("d-none");
-            if (myDropzone.files[1] == null) return;
-            myDropzone.removeFile(myDropzone.files[0]);
-        });
 
-        document.getElementById('submit-button').addEventListener("click", function (e) {
-            e.preventDefault();
-            myDropzone.processQueue();
-        });
-
-        myDropzone.on("success", function () {
-            function redirectUser() {
-                location.reload();
-            }
-            setInterval(redirectUser, 1500);
-        });
-
-        myDropzone.on('sending', function(file, xhr, formData) {
-            // Append inputs to FormData 
-            document.querySelector(".dz-progress").classList.remove("d-none");
-            formData.append("courseThumbnail", document.getElementById('courseThumbnail').value);
-        });
-    }
-}; */
 
 function readURL(input) {
     if (input.files && input.files[0]) {
--- src/static/scripts/upload_lesson.js
+++ src/static/scripts/upload_lesson.js
@@ -5,57 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
         imageForm.classList.toggle("d-none");
     });
 });
-// Dropzone.js for segmenting data payload to chunks of data
-// Useful resources:
-// https://stackoverflow.com/questions/46728205/dropzone-submit-button-on-upload/46732882
-// https://docs.dropzone.dev/
-
-//Dropzone for lesson thumbnail
-// Dropzone.options.dropper = {
-//     maxFiles: 1,
-//     paramName: 'lessonThumbnail',
-//     acceptedFiles: ".jpeg,.jpg,.png",
-//     chunking: true,
-//     forceChunking: true,
-//     url: '/upload_lesson',
-//     maxFilesize: 50, // megabytes
-//     chunkSize: 1000000, // bytes
-//     retryChunks: true,
-//     retryChunksLimit: 3,
-//     autoProcessQueue: false,
-
-//     init: function() {
-//         let lessonThumbnailDropzone = this;
-        
-//         // when the user uploads more than one image, this function will remove the old lesson thumbnail and replace it with the new lesson thumbnail that was added by the user
-//        lessonThumbnailDropzone.on("addedfile", function() {
-//             $(".dz-progress").hide();
-//             if (lessonThumbnailDropzone.files[1] == null) return;
-//            lessonThumbnailDropzone.removeFile(userProfileImageDropzone.files[0]);
-//         });
-
-//         // tells dropzone to upload the image data to the web server when the user clicks on the upload button
-//         document.getElementById('submit-button').addEventListener("click", function (e) {
-//             e.preventDefault();
-//            lessonThumbnailDropzone.processQueue();
-//         });
-
-//         // sending the chunks of data when user clicks on the upload button
-//        lessonThumbnailDropzone.on('sending', function(file, xhr, formData) {
-//             /* Append inputs to FormData */
-//             $(".dz-progress").show();
-//             formData.append("lessonThumbnail", document.getElementById('dropper').value);
-//         });
-        
-//         // when the user profile image has been successfully uploaded, refresh the page after 1.5 seconds
-//        lessonThumbnailDropzone.on("success", function () {
-//             function redirectUser() {
-//                 location.reload();
-//             }
-//             setInterval(redirectUser, 1500);
-//         });
-//     }
-// };
 
 function readURL(input) {
     if (input.files && input.files[0]) {
@@ -138,48 +87,6 @@ Dropzone.options.dropper = {
 };
 
 
-//     init: function() {
-//         let lessonVideoDropzone = this;
-        
-//         // when the user uploads more than one video, this function will remove the old lesson video and replace it with the new lesson video that was added by the user
-//        lessonVideoDropzone.on("addedfile", function() {
-//             $(".dz-progress").hide();
-//             if (lessonVideoDropzone.files[1] == null) return;
-//            lessonVideoDropzone.removeFile(lessonVideoDropzone.files[0]);
-//         });
-
-//         // this tells dropzone to upload the video data to the web server when the user clicks on the upload button
-//         document.getElementById('submit-button').addEventListener("click", function (e) {
-//             e.preventDefault();
-//            lessonVideoDropzone.processQueue();
-//         });
-
-//         // sending the chunks of data when user clicks on the upload button
-//        lessonVideoDropzone.on('sending', function(file, xhr, formData) {
-//             /* Append inputs to FormData */
-//             $(".dz-progress").show();
-//             formData.append("lessonVideo", document.getElementById('dropper').value);
-//         });
-        
-//         // when the lesson video has been successfully uploaded, refresh the page after 1.5 seconds
-//        lessonVideoDropzone.on("success", function () {
-//             function redirectUser() {
-//                 location.reload();
-//             }
-//             setInterval(redirectUser, 1500);
-//         });
         
         
 
-//         
-//     }
-// };
-
-//  var lessonVideoDropzone = new Dropzone(document.getElementById("formDropZone", {
-//     // options here
-//  });
-
-// // Update the total progress bar
-// lessonVideoDropzone.on("totaluploadprogress", function(progress) {
-//    // add code to edit bootstrap 5 progress
-// });
