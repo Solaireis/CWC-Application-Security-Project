454a0b3eddbb29e0ab65e86219cf5fd6cb9c7d5d
KJHJason <kuanjunhaojason@gmail.com>
2022-08-19 00:12:49 +0800

    frontend changes for 2FA

--- src/static/scripts/2fa.js
+++ src/static/scripts/2fa.js
@@ -2,3 +2,16 @@ let secretToken = document.getElementById("secretToken");
 secretToken.addEventListener("click", function() {
     navigator.clipboard.writeText(secretToken.innerText);
 });
+
+let secretTokenBtn = document.getElementById("secretTokenBtn");
+let secretTokenDiv = document.getElementById("secretTokenDiv");
+secretTokenBtn.addEventListener("click", function() {
+    if (secretTokenDiv.classList.contains("d-none")) {
+        secretTokenBtn.innerText = "Hide Setup Key"
+        secretTokenDiv.classList.remove("d-none");
+    }
+    else {
+        secretTokenBtn.innerText = "View Setup Key"
+        secretTokenDiv.classList.add("d-none");
+    }
+});
